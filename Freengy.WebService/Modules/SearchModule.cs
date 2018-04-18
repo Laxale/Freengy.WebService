e12f4c142bfa382e435ee00c21f77f590e858b98
Laxale <axel.1777@mail.ru>
2018-04-18 21:50:24 +0300

    Added initial (not working yet) searching module implementation

---
+++ Freengy.WebService/Modules/SearchModule.cs
@@ -0,0 +1,42 @@
+﻿// Created by Laxale 18.04.2018
+//
+//
+
+using System;
+
+using Nancy;
+
+
+namespace Freengy.WebService.Modules 
+{
+    /// <summary>
+    /// Searching module (whatever - users, session, etc).
+    /// </summary>
+    public class SearchModule : NancyModule 
+    {
+        public SearchModule() 
+        {
+            Console.WriteLine($"Created { nameof(SearchModule) }");
+
+            Get[Subroutes.Search.Root] = OnRootSearchRequest;
+
+            Post[Subroutes.Search.SearchUsers] = OnSearchUsersRequest;
+        }
+
+
+        private dynamic OnRootSearchRequest(dynamic arg) 
+        {
+            Console.WriteLine("Got root searching request");
+
+            return $"Use {Subroutes.Search.SearchUsers} to search for users";
+        }
+
+        private dynamic OnSearchUsersRequest(dynamic arg) 
+        {
+            var 
+            Console.WriteLine("Got searching user request");
+
+            throw new NotImplementedException();
+        }
+    }
+}
--- Freengy.WebService/Subroutes.cs
+++ Freengy.WebService/Subroutes.cs
@@ -11,18 +11,35 @@ namespace Freengy.WebService
     internal class Subroutes 
     {
         /// <summary>
-        /// Hello subroute.
+        /// Hello module subroute.
         /// </summary>
         public static readonly string Hello = "/hello";
 
         /// <summary>
-        /// Register new user subroute.
+        /// Registration module subroute.
         /// </summary>
         public static readonly string Register = "/register";
 
         /// <summary>
-        /// Log in subroute.
+        /// Log-in module subroute.
         /// </summary>
         public static readonly string Login = "/login";
+
+
+        /// <summary>
+        /// Contains search module subroutes.
+        /// </summary>
+        public static class Search 
+        {
+            /// <summary>
+            /// Root Search module subroute.
+            /// </summary>
+            public static readonly string Root = "/search";
+
+            /// <summary>
+            /// Search users subroute.
+            /// </summary>
+            public static readonly string SearchUsers = $"{ Search.Root }/users";
+        }
     }
 }
